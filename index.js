e0b4eac09533d66a58089a5319e1a8d9d7457121
Tiffany Lam <tlam5@sbcglobal.net>
2017-03-19 23:24:26 -0700

    Begin pseudocoding logic

---
+++ index.js
@@ -0,0 +1,7 @@
+// generate a random number, figure out next button that lights up based on number
+// add to sequence by pushing that button into an array
+
+// push user's button presses into an array
+// compare original light up array with user presses array
+
+// link sounds to play button clicks
