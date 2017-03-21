91c54f638c84cf90cd686d7787d9aac4537b8561
Tiffany Lam <tlam5@sbcglobal.net>
2017-03-21 00:14:45 -0700

    Add lightup class to buttons in random order

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-// generate a random number, figure out next button that lights up based on number
+var demoArr =[];
+var count = 1;
+
+// for each count, generate a random number, figure out next button that lights up based on number
 // add to sequence by pushing that button into an array
+var random = Math.random();
+if (random < 0.25) {
+  demoArr.push('one');
+} else if (random < 0.5) {
+  demoArr.push('two');
+} else if (random < 0.75) {
+  demoArr.push('three');
+} else if (random < 1) {
+  demoArr.push('four');
+}
+
+// go through that array, make the buttons light up
+  // add class to button and then remove it
+for (var i = 0; i < demoArr.length; i++) {
+  document.getElementById(`${demoArr[i]}`).classList.add('lightup');
+}
 
 // push user's button presses into an array
 // compare original light up array with user presses array
