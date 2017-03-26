070ba7b45e0a77ee964d4837969eee18fe9fecda
Tiffany Lam <tlam5@sbcglobal.net>
2017-03-25 22:09:36 -0700

    Pull randomizer and computer light-ups into separate functions

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
 var computerPlays =[];
 var count = 0;
 
-// increment count, update display
-count++;
-var countNumber = document.getElementById('count__number');
-// countNumber.innerHTML = count;
-
-// for each count, generate a random number, figure out next button that lights up based on number
-// add to sequence by pushing that button into an array
-var random = Math.random();
-if (random < 0.25) {
-  computerPlays.push('blue');
-} else if (random < 0.5) {
-  computerPlays.push('red');
-} else if (random < 0.75) {
-  computerPlays.push('green');
-} else if (random < 1) {
-  computerPlays.push('yellow');
+function getComputerPlays() {
+  // for each count, generate a random number, figure out next button that will light up based on number
+  // add to sequence by pushing that button into an array
+  var random = Math.random();
+  if (random < 0.25) {
+    computerPlays.push('blue');
+  } else if (random < 0.5) {
+    computerPlays.push('red');
+  } else if (random < 0.75) {
+    computerPlays.push('green');
+  } else if (random < 1) {
+    computerPlays.push('yellow');
+  }
 }
 
-// go through that array, make the buttons light up
-  // add class to button and then remove it
-computerPlays.map(function(play) {
-  var playBtn = document.getElementById(play);
-  playBtn.classList.add('lightup');
-  setTimeout(function() {
-    playBtn.classList.remove('lightup');
-  }, 1000);
-});
+function runComputerPlays() {
+  // go through that array, make the buttons light up
+    // add class to button and then remove it
+  computerPlays.map(function(play) {
+    var playBtn = document.getElementById(play);
+    playBtn.classList.add('lightup');
+    setTimeout(function() {
+      playBtn.classList.remove('lightup');
+    }, 1500);
+  });
+}
 
+function startGame() {
+  // increment count, update display
+  count++;
+  var countDisplay = document.getElementById('count__number');
+  countDisplay.innerHTML = count;
 
-// push user's button presses into an array
-// compare original light up array with user presses array
+  getComputerPlays();
+  runComputerPlays();
 
-// link sounds to play button clicks
+  // push user's button presses into an array
+  // compare original light up array with user presses array
+
+  // link sounds to play button clicks
+}
